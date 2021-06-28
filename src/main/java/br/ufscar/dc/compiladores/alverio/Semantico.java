d8e02b0a7c4af6b30a511df148f0eed94bdaf88f
Eduardo Oliverio <dudu_oliverio@hotmail.com>
2021-06-28 01:42:36 -0300

    verificação semantica

--- src/main/java/br/ufscar/dc/compiladores/alverio/Semantico.java
+++ src/main/java/br/ufscar/dc/compiladores/alverio/Semantico.java
@@ -29,10 +29,25 @@ public class Semantico extends AlverioBaseVisitor<Void> {
 	public Void visitTestcase(TestcaseContext ctx) {
 		String elementName = ctx.condition().element.getText();
 		String eventName = ctx.condition().event.getText();
-//		if (!entityList.contains(elementName)) {
-//			System.err.println("Entity " + elementName + " not declared");
-//			hasError = true;
-//		}
+		
+//		faça um for em entityList
+//		para cada elemento, verifique se elementName está em entityList
+//		tem que comparar pelo nome, usando equals, o contains não funciona mais pois a gente mudou de string para EntityStruct, lembra?).
+//		Se não estiver, dê erro de entidade não declarada
+//		se estiver, faça um for em entityList.events e verifique se eventName está em entityList.events.
+//		Se não estiver, dê erro de entidade não declarada.
+		
+		for(EntityStruct es : entityList) {
+			if(!es.name.equals(elementName)) {
+				System.err.println("Entity " + elementName + " not declared");
+				hasError = true;
+			}
+			else if(es.events.equals(eventName)) {
+				System.err.println("Event " + eventName + " not declared");
+				hasError = true;
+			}
+		}
+			
 		return null;
 	}
 }
